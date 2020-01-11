4f27853721d612fe1520561c40cf4cd0b89dc384
Hüseyin İyibaş <huseyiniyibas@gmail.com>
2020-01-11 21:24:55 +0300

    Facebook Authentication is done.

--- Fasher/AppDelegate.swift
+++ Fasher/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Hüseyin İyibaş. All rights reserved.
 //
 
+import Firebase
 import UIKit
 
 @UIApplicationMain
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
         return true
     }
 
---
+++ Fasher/Services/Authentication.swift
@@ -0,0 +1,9 @@
+//
+//  Authentication.swift
+//  Fasher
+//
+//  Created by Hüseyin İyibaş on 11.01.2020.
+//  Copyright © 2020 Hüseyin İyibaş. All rights reserved.
+//
+
+import Foundation
